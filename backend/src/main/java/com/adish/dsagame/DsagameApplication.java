46e206fb7a57593872f3288371f8284f7eb4bf86
Adish Jain <adnihusat@gmail.com>
2026-02-20 12:04:12 +0530

    Initial backend setup

---
+++ backend/src/main/java/com/adish/dsagame/DsagameApplication.java
@@ -0,0 +1,13 @@
+package com.adish.dsagame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsagameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsagameApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/adish/dsagame/DsagameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adish.dsagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsagameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
